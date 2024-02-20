d37e47ceda004f5b4f32c7d660c50659efb30a5a
Tristan C <torisutansan02@gmail.com>
2024-02-20 13:06:15 -0800

    Adding necessary changes to css file

--- bands-boutique/src/Navbar.js
+++ bands-boutique/src/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 fixed top-0 w-full z-10">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
-          <div className="text-white font-bold text-xl">My React App</div>
+          <div className="text-white font-bold text-xl">My Online Shop</div>
           <div className="space-x-4">
             <a href="#" className="text-white">Home</a>
-            <a href="#" className="text-white">About</a>
+            <a href="#" className="text-white">Shop</a>
             <a href="#" className="text-white">Contact</a>
           </div>
         </div>
