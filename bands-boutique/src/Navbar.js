a71784e83cbf82f11293689f38d83d4c0ea39ea9
Tristan C <torisutansan02@gmail.com>
2024-02-20 13:33:13 -0800

    Adding a working navbar with working Tailwind CSS

--- bands-boutique/src/Navbar.js
+++ bands-boutique/src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white p-4 shadow-md fixed top-0 w-full z-10">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-gray-700 font-bold text-xl">My Fashion Store</div>
+        <div className="text-gray-700 font-bold text-xl">Bands Boutique</div>
         <div className="flex space-x-6"> {/* Increased spacing with space-x-6 */}
           <a href="#" className="text-gray-700 hover:text-black">Home</a>
           <a href="#" className="text-gray-700 hover:text-black">Shop</a>
--- bands-boutique/tailwind.config.js
+++ bands-boutique/tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/index.js",
+    "./src/Navbar.js",
     "./public/index.html",
   ],
   // Other Tailwind CSS configurations...
