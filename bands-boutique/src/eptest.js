67ff17bfe3538ba0529195bb20b5f3de3111706d
billblaster <102612368+billblaster@users.noreply.github.com>
2024-02-20 16:00:52 -0800

    messed with servers a bit

--- bands-boutique/src/eptest.js
+++ bands-boutique/src/eptest.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./Models/users');
 require('dotenv').config();
+const cors = require('cors')
 
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const userRoute = express.Router();
+userRoute.use(express.json())
+userRoute.use(cors())
 
 userRoute.get("/getUsers", async (req, res) => {
     try {
@@ -20,6 +23,15 @@ userRoute.get("/getUsers", async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+userRoute.post("/createUser", async (req, res)=> {
+    const user = req.body
+    const newUser = new UserModel(user)
+    await newUser.save()
+
+    res.json(user)
+
+})
 
 app.use('/', userRoute);
 
