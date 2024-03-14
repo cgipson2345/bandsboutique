a750006b9b68395fef24c015cf1825217599a480
Tristan C <torisutansan02@gmail.com>
2024-03-14 00:30:07 -0700

    Adding user authentication, working fr this time

--- bands-boutique/src/Home.js
+++ bands-boutique/src/Home.js
@@ -8,19 +8,7 @@ const Home = () => {
       <div className= "flex flex-row  justify-center  justify-items-center gap-x-10 m-10 items-center">
         <div className="left">
           <img src ={ringtree} alt ="dog"/>
-
         </div>
-        <div className="right flex-col justify-center justify-items-center items-center">
-          <h1 className="font-bold text-3xl ">Welcome</h1>
-          <p>Email Adress</p>
-          <div className="border-2 rounded-md px-28 py-5 border-black mb-2"> </div>
-          <p>Password</p>
-          <div className="border-2 rounded-md px-28 py-5 border-black mb-2"></div>
-          <button className='bg-neutral-950  text-white px-28 py-3 rounded-md'>Login</button>
-
-
-        </div>
-
       </div>
     </div>
   );
--- bands-boutique/src/Login.js
+++ bands-boutique/src/Login.js
@@ -1,21 +1,13 @@
-// src/components/Home.js
-import React from 'react';
+// Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
   return (
-  <div className="flex flex-row justify-center items-center h-screen">
-    <div className="flex justify-center items-center">
-      <img src="rings.png" alt="rings image" className="size-full"/>
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the home page content. Customize it as needed for your application.</p>
     </div>
-    <div className="flex flex-col justify-center items-center ml-10">
-      <h1 className="mb-4 text-3xl font-bold">Welcome!</h1>
-      <p className="text-lg">Email Address</p>
-      <input type="text" placeholder="" className="mb-4 w-40 border-2 rounded-md"/>
-      <p className="text-lg">Password</p>
-      <input type="password" placeholder="" className="mb-4 w-40 border-2 rounded-md"/>
-      <button type="button" className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-40">Login</button>
-    </div>
-  </div>
   );
 };
 
